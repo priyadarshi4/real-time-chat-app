42e03e7675a32032b5e0f16f581a6f999b04b756
Priyadarshi prince <priyadarshiprince5@gmail.com>
2024-10-26 18:46:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.get("/",(req,res)=>{
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
